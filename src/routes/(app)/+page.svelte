e4bc68f11a0d8898ae2ff2ab1347e2c49752d40c
ztoms <tommyzhao111@gmail.com>
2022-12-07 19:49:33 -0800

    refactor firebase init

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -3,34 +3,13 @@
 	import FaGoogle from 'svelte-icons/fa/FaGoogle.svelte';
 	import FaTwitter from 'svelte-icons/fa/FaTwitter.svelte';
 	// Import the functions you need from the SDKs you need
-	import { initializeApp } from 'firebase/app';
-	import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-	// TODO: Add SDKs for Firebase products that you want to use
-	// https://firebase.google.com/docs/web/setup#available-libraries
-	// Your web app's Firebase configuration
-	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-	const firebaseConfig = {
-		apiKey: 'AIzaSyB1iV7lXhmJgot5vRR0Ear0ORv50WBcRoE',
-		authDomain: 'real-0.firebaseapp.com',
-		projectId: 'real-0',
-		storageBucket: 'real-0.appspot.com',
-		messagingSenderId: '108552382150',
-		appId: '1:108552382150:web:0d66a8bafd46aaae71d9c7',
-		measurementId: 'G-QPVNB9D1EB'
-	};
 
 	let email = '';
 	let password = '';
 
-	// Initialize Firebase
-	const app = initializeApp(firebaseConfig);
-
 	// login function
 	const login = () => {
 		console.log('login');
-		const auth = getAuth();
-		console.log(auth);
-		auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
 	};
 
 	const logout = () => {
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '$lib/global.css';
+
+	import { initializeApp } from 'firebase/app';
+	import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+	// TODO: Add SDKs for Firebase products that you want to use
+	// https://firebase.google.com/docs/web/setup#available-libraries
+	// Your web app's Firebase configuration
+	// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+	// let email = '';
+	// let password = '';
+
+	// // Initialize Firebase
+	// const app = initializeApp(firebaseConfig);
 </script>
 
 <slot />
